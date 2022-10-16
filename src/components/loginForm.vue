4bd02cf7feab5023116f9bb5fdb4e03799c16481
cocohyuncoco <guswjd8287@gmail.com>
2022-10-16 17:51:34 +0900

    [임현정] 로그인 폼 컴포넌트 생성 및 마크업

---
+++ src/components/loginForm.vue
@@ -0,0 +1,27 @@
+<template>
+  <form action="">
+    <div>
+      <label for="username">id: </label>
+      <input type="text" />
+    </div>
+    <div>
+      <label for="password">pw: </label>
+      <input type="text" />
+    </div>
+    <button type="submit">로그인</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+
+  mounted() {},
+
+  methods: {},
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/views/LoginPage.vue
+++ src/views/LoginPage.vue
@@ -1,9 +1,17 @@
 <template>
-  <div>Login</div>
+  <div>
+    <h1>로그인</h1>
+    <loginForm></loginForm>
+  </div>
 </template>
 
 <script>
+import loginForm from '@/components/loginForm.vue';
+
 export default {
+  components: {
+    loginForm,
+  },
   data() {
     return {};
   },
