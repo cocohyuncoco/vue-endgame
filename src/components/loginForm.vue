7299d766501f474aaffb375eb5216e658ee45512
cocohyuncoco <guswjd8287@gmail.com>
2023-01-18 13:13:33 +0900

    [임현정] email, passwore from에 validation 체크기능 추가 (email 형식이 아닐시 로그인 버튼 비활성화)

--- src/components/loginForm.vue
+++ src/components/loginForm.vue
@@ -2,19 +2,22 @@
   <form @submit.prevent="submitForm">
     <div>
       <label for="username">id: </label>
-      <input type="text" v-model="username" />
+      <input id="username" type="text" v-model="username" />
     </div>
     <div>
       <label for="password">pw: </label>
-      <input type="password" v-model="password" />
+      <input id="password" type="password" v-model="password" />
     </div>
-    <button type="submit">로그인</button>
+    <button :disabled="!isUsernameValid || !password" type="submit">
+      로그인
+    </button>
     <p>{{ logMessage }}</p>
   </form>
 </template>
 
 <script>
 import { loginUser } from '@/api/index';
+import { validateEmail } from '@/utils/validation';
 
 export default {
   data() {
@@ -26,7 +29,12 @@ export default {
   },
 
   mounted() {},
-
+  computed: {
+    // 이 속성은 데이터 변화에 따라서 내용을 자동으로 계산해주는 연산 식
+    isUsernameValid() {
+      return validateEmail(this.username);
+    },
+  },
   methods: {
     async submitForm() {
       try {
@@ -38,10 +46,13 @@ export default {
         const { data } = await loginUser(userData);
         console.log(data.user.nickname);
         this.logMessage = `${data.user.nickname}님 환영합니다`;
-        this.initFrom();
       } catch (error) {
         // 에러 핸들링할 코드
-        console.log(error.response);
+        // console.log(error.response);
+        this.logMessage = error.response.data;
+      } finally {
+        // ininFrom() 함수가 중복될 경우 finally 함수를 이용해서 끝에다 넣으면 된다
+        this.initFrom();
       }
     },
     initFrom() {
